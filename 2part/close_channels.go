85ffbd17fd3149b224de63fc56ada8fb02a79674
Vlad <vladvolkov03@mail.ru>
2022-07-17 20:16:02 +0300

    close channels

--- 2part/close_channels.go
+++ 2part/close_channels.go
@@ -1,5 +1,18 @@
 package main
 
+import "fmt"
+
 func main(){
-	
+	ch:= make(chan int,3)
+	ch <- 123
+	ch <- 19
+	close(ch)
+
+	for i:= 0;i < cap(ch);i++{
+		if val,opened := <-ch;opened{
+			fmt.Println(val)
+		} else {
+			fmt.Println("channel closed")
+		}
+	}
 }
