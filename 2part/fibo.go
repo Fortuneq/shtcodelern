25f7b8dc4cd98536f086e16af7206bbde52ab51f
Vlad <vladvolkov03@mail.ru>
2022-07-17 20:22:14 +0300

    fibo v2

---
+++ 2part/fibo.go
@@ -0,0 +1,19 @@
+package main
+
+import "fmt"
+
+func main(){
+	ch := make(chan int)
+
+	go factorial(5,ch)
+	
+	fmt.Println(<-ch)
+}
+
+func factorial(n int, ch chan int ){
+	result := 1
+    for i:=1; i <= n; i++{
+        result *= i
+    }
+    ch <- result
+}
