0f406261207e238c0248bfa778985402dd304a5b
Vlad <vladvolkov03@mail.ru>
2022-07-18 20:14:15 +0300

    postgre connected

--- part5/main.go
+++ part5/main.go
@@ -1,14 +1,33 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+
+	_ "github.com/lib/pq"
+)
 
 const (
-	host = "localhost"
-	port = 5432
-	user = "postgres"
-	//password = "537j04222"
-	dbname = "postgres"
+	host     = "localhost"
+	port     = 5432
+	user     = "postgres"
+	password = "537j04222"
+	dbname   = "postgres"
 )
-func main(){
-	fmt.Println(host,port,user,dbname)
+
+func main() {
+	psqlconn := fmt.Sprintf("host = %s port = %d user = %s password = %s dbname = %s", host, port, user, password, dbname)
+
+	db, err := sql.Open("postgres", psqlconn)
+	tryerr(err)
+	inserstJackhm := `insert into "clients"("id","first_name","last_name","role") values($1,$2,$3,$4)`
+	_,g := db.Exec(inserstJackhm ,4,"Jack","London","Programmer")
+	tryerr(g)
+	defer db.Close()
+}
+
+func tryerr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
