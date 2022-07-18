24006c19582a878fd772e02cece43e2e0e5ef46d
Vlad <vladvolkov03@mail.ru>
2022-07-18 19:21:39 +0300

    added docker

--- part5/main.go
+++ part5/main.go
@@ -1,12 +1,14 @@
 package main
 
+import "fmt"
+
 const (
 	host = "localhost"
 	port = 5432
 	user = "postgres"
-	//password = "537j"
+	//password = "537j04222"
 	dbname = "postgres"
 )
 func main(){
-
+	fmt.Println(host,port,user,dbname)
 }
